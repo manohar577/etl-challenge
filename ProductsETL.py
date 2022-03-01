e8f4278173f75c4f812f50fb42c8155f326063fb
Phani Sai Manohar Gandrakota <manu.abhi57@gmail.com>
2022-02-28 22:24:42 -0500

    Add files via upload

---
+++ ProductsETL.py
@@ -0,0 +1,18 @@
+productFile = open('products.txt', 'r',encoding="UTF-8")
+productLines = productFile.readlines()
+
+targetFile = open('targets.txt', 'r', encoding="UTF-8")
+targetLines = targetFile.readlines()
+
+targetValuesDict = {line.split("\t")[0] : line.rstrip().split("\t")[1] for line in targetLines}
+
+templeteList = [ templete for templete in productLines[0].rstrip().split("|")]
+
+for line in productLines[1:]:
+    valuesList = [value for value in line.rstrip().split("|")]
+    productDict = dict(zip(templeteList[:3], valuesList[:3] ))
+    productDict[templeteList[-1:][0]] = float(valuesList[-1:][0][1:].replace(',', ""))
+    taxonomy = [templeteList[item][:3]+valuesList[item] for item in range(3,7)]
+    productDict["taxonomy"] = taxonomy
+    productDict["targetValues"] = [targetValuesDict.get(item) for item in taxonomy]
+    print (productDict)
